36c751fbbad7e472575ad89577d24c3720c998ee
Roland Foucher <roland.foucher.pro@gmail.com>
2022-03-30 11:55:32 +0200

    save new faceSnap init

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { filter, map, Observable, switchMap } from 'rxjs';
 import { FaceSnap } from '../models/face-snap.model';
 
 // root - instancié une fois au root de l'application
@@ -6,91 +8,46 @@ import { FaceSnap } from '../models/face-snap.model';
   providedIn: 'root',
 })
 export class FaceSnapService {
-  faceSnaps: FaceSnap[] = [
-    {
-      id: 1,
-      title: 'title',
-      description: 'mon facesnap',
-      creatinDate: new Date(),
-      imgURL:
-        'https://tse2.mm.bing.net/th?id=OIP.LXWCLJ9xu7dCkL_53aodqAHaJ4&pid=Api',
-      snaps: 100,
-      location: 'Lyon',
-    },
-    {
-      id: 2,
-      title: 'mon snape',
-      description: "c'est chouette",
-      creatinDate: new Date(),
-      imgURL:
-        'https://tse2.mm.bing.net/th?id=OIP.XpacSt8FfbgS25OVYrFmEQHaHa&pid=Api',
-      snaps: 12,
-      location: 'Paris',
-    },
-    {
-      id: 3,
-      title: 'hohohoh',
-      description: "c'est la fête",
-      creatinDate: new Date(),
-      imgURL:
-        'https://tse4.mm.bing.net/th?id=OIP.5x-WzZJv-2nYCgYCgj4U2QHaFh&pid=Api',
-      snaps: 200,
-    },
-    {
-      id: 4,
-      title: 'title',
-      description: 'mon facesnap',
-      creatinDate: new Date(),
-      imgURL:
-        'https://tse2.mm.bing.net/th?id=OIP.LXWCLJ9xu7dCkL_53aodqAHaJ4&pid=Api',
-      snaps: 10,
-      location: 'Lyon',
-    },
-    {
-      id: 5,
-      title: 'mon snape',
-      description: "c'est chouette",
-      creatinDate: new Date(),
-      imgURL:
-        'https://tse2.mm.bing.net/th?id=OIP.XpacSt8FfbgS25OVYrFmEQHaHa&pid=Api',
-      snaps: 12,
-      location: 'Paris',
-    },
-    {
-      id: 6,
-      title: 'hohohoh',
-      description: "c'est la fête",
-      creatinDate: new Date(),
-      imgURL:
-        'https://tse4.mm.bing.net/th?id=OIP.5x-WzZJv-2nYCgYCgj4U2QHaFh&pid=Api',
-      snaps: 14,
-    },
-  ];
+  constructor(private http: HttpClient) {}
 
-  getAllFaceSnaps(): FaceSnap[] {
-    return this.faceSnaps;
+  getAllFaceSnaps(): Observable<FaceSnap[]> {
+    return this.http.get<FaceSnap[]>('http://localhost:3000/facesnaps');
   }
 
-  getFaceSnapById(faceSnapId: number): FaceSnap {
-    const faceSnap = this.faceSnaps.find(
-      (faceSnap) => faceSnap.id === faceSnapId
+  getFaceSnapById(faceSnapId: number): Observable<FaceSnap> {
+    return this.http.get<FaceSnap>(
+      'http://localhost:3000/facesnaps/' + faceSnapId
     );
-    if (faceSnap) {
-      return faceSnap;
-    }
-    throw new Error('faceSnap not found !');
   }
 
   // pour apprendre le type litteral
-  snapFaceSnapById(faceSnapId: number, snapType: 'snape' | 'unsnap'): void {
-    const faceSnap = this.getFaceSnapById(faceSnapId);
-    snapType === 'snape' ? faceSnap.snaps++ : faceSnap.snaps--;
+  snapFaceSnapById(
+    faceSnapId: number,
+    snapType: 'snape' | 'unsnap'
+  ): Observable<FaceSnap> {
+    return this.getFaceSnapById(faceSnapId).pipe(
+      map((faceSnap) => ({
+        ...faceSnap,
+        snaps: faceSnap.snaps + (snapType === 'snape' ? 1 : -1),
+      })),
+      switchMap((updatedFaceSnap) =>
+        this.http.put<FaceSnap>(
+          'http://localhost:3000/facesnaps/' + faceSnapId,
+          updatedFaceSnap
+        )
+      )
+    );
   }
 
-  addFaceSnap(faceSnap: FaceSnap): void {
-    faceSnap.id = this.faceSnaps[this.faceSnaps.length - 1].id++;
-    faceSnap.snaps = 0;
-    faceSnap.creatinDate = new Date();
-    this.faceSnaps.push(faceSnap);
+  addFaceSnap(faceSnap: FaceSnap): Observable<FaceSnap> {
+    // return this.getAllFaceSnaps().pipe(
+    //   filter()
+    // )
+    // faceSnap.id = this.getAllFaceSnaps().pipe(
+    // )
+    // [this.faceSnaps.length - 1].id++;
+    // faceSnap.snaps = 0;
+    // faceSnap.creatinDate = new Date();
+    // this.faceSnaps.push(faceSnap);
   }
 }
